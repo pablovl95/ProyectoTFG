2cf3c973f00622274f466db1641cead63ae811a4
PABLO VERA LOPEZ <pabverlop1@alum.us.es>
2024-05-12 15:18:37 +0200

    fix: jsx components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Login from "./components/Login.jsx";
-import  Navbar  from "./components/Navbar.jsx";
-import  About  from "./screens/About.jsx";
-import  Home  from "./screens/Home.jsx";
-import  Search  from "./screens/Search.jsx";
-import Product from "./screens/Product.jsx";
+import Login from "./components/Login";
+import  Navbar  from "./components/Navbar";
+import  About  from "./screens/About";
+import  Home  from "./screens/Home";
+import  Search  from "./screens/Search";
+import Product from "./screens/Product";
 
 function App() {
   const [login, setLogin] = useState(false);
