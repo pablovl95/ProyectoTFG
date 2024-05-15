841272aab3adc41eef8aeb1a0972555c966dcebe
PABLO VERA LOPEZ <pabverlop1@alum.us.es>
2024-05-15 20:14:09 +0200

    add: popularproducts home

--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ProductSlider from '../components/ProductSlider';
-
+import ProductCard from '../components/productCard';
 import "./css/Home.css"; // Importa tus estilos CSS aquí
 function Home() {
-
-
+const [popularProducts, setPopularProducts] = useState([]);
+useEffect(() => {
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://localhost:5000/api/v1/popularProducts');
+      const data = await response.json();
+      console.log(data);
+      setPopularProducts(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }  
+  fetchData();
+}, []);
   const sliderList = [
     {
       "id": 1,
@@ -27,11 +39,16 @@ function Home() {
     <div className="HomeContainer">
       <div className="Slider">
         <ProductSlider sliderList={sliderList} className="Slider" />
-      </div>
+      </div> 
       <div className='data-container'>
         <h1 className="HomeTitle">
           Productos más vendidos
         </h1>
+        <div className="data-container-cards">
+          {popularProducts.map((product) => (
+            <ProductCard key={product.ProductID} product={product} />
+          ))}
+      </div>
       </div>
 
     </div>
